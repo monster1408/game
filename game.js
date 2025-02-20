313966d2e6122cd7caf1d3c4a4a7ebccbb67cd7a
monster1408 <yukiya.toyohara@hotmail.com>
2025-02-20 09:37:54 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var name = prompt("1文字");
 var player = document.getElementByid("player");
 if (name.length == 1) {
-  document.getElementByid("player").innerHTML = "a";
+  document.getElementById("player").innerHTML = "a";
 }
 
 document.getElementById("player").innerHTML = name;
