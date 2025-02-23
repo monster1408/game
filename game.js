a8bfaee44529b2f6aeda17faa039eb0cfcfda772
monster1408 <yukiya.toyohara@hotmail.com>
2025-02-23 22:29:07 +0900

    game.js を更新
    var stage = [0,1,0,1,1,0,0,1]; for (var i = 0;  i < stage.length; i++) {   stage = stage.replace(/1/,g,地面.png);   document.write("<img id="+i+" class="stage" src="+stage+">"); }

--- game.js
+++ game.js
@@ -5,8 +5,4 @@ while (name.length !== 1) {
 }
 player.innerHTML = name;
 
-var stage = [0,1,0,1,1,0,0,1];
-for (var i = 0;  i < stage.length; i++) {
-  stage = stage.replace(/1/,g,地面.png);
-  document.write("<img id="+i+" class="stage" src="+stage+">");
-}
+
