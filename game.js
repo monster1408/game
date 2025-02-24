ccfed98acc0a604b1f052ece8df8a567c06c2a6d
monster1408 <yukiya.toyohara@hotmail.com>
2025-02-24 21:17:20 +0900

    game.js を更新

--- game.js
+++ game.js
@@ -1,8 +1,23 @@
 var name = prompt("1文字");
 var player = document.getElementById("player");
+var stageElement = document.getElementById("stage");
 while (name.length !== 1) {
   name = prompt("1文字");
 }
-player.innerHTML = name;
+player.innerHTML=name;
+
+var imgName="";
+var stage = [0,1,0,1,1,0,0,1];
+for (var i = 0; i < stage.length; i++) {
+  if (stage[i] === 1) {
+    imgName += "<img src='地面.png' class='stage'>";  // 1の場合、地面画像
+  } else {
+    imgName += "<img src='空.png' class='stage'>";  // 0の場合、空画像
+  }
+}
+stageElement.innerHTML=imgName
+
+
+
 
 
